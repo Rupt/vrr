e24ab18c441a07b9cc3f2943c068dcd3b32d15c1
Rupert Tombs <rupert.tombs@gmail.com>
2025-01-05 11:11:53 +0000

    Structured test output

--- source/test_hello.c
+++ source/test_hello.c
@@ -6,6 +6,16 @@ int main() {
     int a = 1;
     int b = plus_one(a);
     if (b != a + 1) {
-        fprintf(stderr, "plus_one(%d) != %d\n", a, a + 1);
+        printf(
+            "{"
+            "\"file\": \"%s\", "
+            "\"line\": \"%d\", "
+            "\"expected\": \"%d\", "
+            "\"observed\": \"%d\"}\n",
+            __FILE__,
+            __LINE__,
+            b,
+            a + 1
+        );
     }
 }
