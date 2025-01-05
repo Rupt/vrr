8c0c97ab2e817bdb8ba4df4b56f6cba22e325155
Rupert Tombs <rupert.tombs@gmail.com>
2025-01-05 16:43:48 +0000

    Unstable over unsafe

--- source/test.h
+++ source/test.h
@@ -1,6 +1,6 @@
-#define VRR_UNSAFE_STRINGIFY_INNER(b) #b
-#define VRR_UNSAFE_STRINGIFY(b) VRR_UNSAFE_STRINGIFY_INNER(b)
-#define VRR_UNSAFE_LOG __FILE__ ":" VRR_UNSAFE_STRINGIFY(__LINE__) ": "
+#define VRR_UNSTABLE_STRINGIFY_INNER(b) #b
+#define VRR_UNSTABLE_STRINGIFY(b) VRR_UNSTABLE_STRINGIFY_INNER(b)
+#define VRR_UNSTABLE_LOG __FILE__ ":" VRR_UNSTABLE_STRINGIFY(__LINE__) ": "
 
-#define vrr_expected(format) (VRR_UNSAFE_LOG "expected: " format "\n")
-#define vrr_observed(format) (VRR_UNSAFE_LOG "observed: " format "\n")
+#define vrr_expected(format) (VRR_UNSTABLE_LOG "expected: " format "\n")
+#define vrr_observed(format) (VRR_UNSTABLE_LOG "observed: " format "\n")
