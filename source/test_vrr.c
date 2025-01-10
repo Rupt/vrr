0587bb7dfee3c85218863ed9c958a87f3b9605db
Rupert Tombs <rupert.tombs@gmail.com>
2025-01-10 12:52:22 +0000

    No need to spell little endian out

--- source/test_vrr.c
+++ source/test_vrr.c
@@ -4,7 +4,7 @@
 #include "test.h"
 #include "vrr.h"
 
-static void test_u32_to_from_u8abcd_little_endian(uint32_t x);
+static void test_u32_to_from_u8abcd(uint32_t x);
 
 int
 main(void)
@@ -12,26 +12,26 @@ main(void)
     // A known case must hold.
     struct vrr_u8abcd const example = {0x0a, 0x1b, 0x2c, 0x3d};
     uint32_t const reference = 0x3d2c1b0a;
-    uint32_t const check = vrr_u32_from_u8abcd_little_endian(example);
+    uint32_t const check = vrr_u32_from_u8abcd(example);
     if (check != reference) {
         printf(vrr_observed("0x%08x"), check);
         printf(vrr_expected("0x%08x"), reference);
     }
-    test_u32_to_from_u8abcd_little_endian(reference);
+    test_u32_to_from_u8abcd(reference);
 
     // Pseudo-random other cases must hold.
-    test_u32_to_from_u8abcd_little_endian(0x243f6a88);
-    test_u32_to_from_u8abcd_little_endian(0x85a308d3);
-    test_u32_to_from_u8abcd_little_endian(0x13198a2e);
-    test_u32_to_from_u8abcd_little_endian(0x03707344);
+    test_u32_to_from_u8abcd(0x243f6a88);
+    test_u32_to_from_u8abcd(0x85a308d3);
+    test_u32_to_from_u8abcd(0x13198a2e);
+    test_u32_to_from_u8abcd(0x03707344);
 }
 
 static void
-test_u32_to_from_u8abcd_little_endian(uint32_t const x)
+test_u32_to_from_u8abcd(uint32_t const x)
 {
-    struct vrr_u8abcd const bytes = vrr_u8abcd_from_u32_little_endian(x);
+    struct vrr_u8abcd const bytes = vrr_u8abcd_from_u32(x);
 
-    // Our little-endian bytes form must match a reference implementation.
+    // Our little-endian layout must match a reference implementation.
     struct vrr_u8abcd const ref = {(x >> 0) & 0xff, (x >> 8) & 0xff,
                                    (x >> 16) & 0xff, (x >> 24) & 0xff};
     if (bytes.a != ref.a || bytes.b != ref.b || bytes.c != ref.c ||
@@ -43,7 +43,7 @@ test_u32_to_from_u8abcd_little_endian(uint32_t const x)
     }
 
     // Our inverse function must reconstruct the original.
-    uint32_t const check = vrr_u32_from_u8abcd_little_endian(bytes);
+    uint32_t const check = vrr_u32_from_u8abcd(bytes);
     if (check != x) {
         printf(vrr_observed("0x%08x"), check);
         printf(vrr_expected("0x%08x"), x);
--- source/vrr.h
+++ source/vrr.h
@@ -8,13 +8,13 @@ struct vrr_u8abcd {
 };
 
 static inline uint32_t
-vrr_u32_from_u8abcd_little_endian(struct vrr_u8abcd x)
+vrr_u32_from_u8abcd(struct vrr_u8abcd x)
 {
     return x.a | (uint32_t)x.b << 8 | (uint32_t)x.c << 16 | (uint32_t)x.d << 24;
 }
 
 static inline struct vrr_u8abcd
-vrr_u8abcd_from_u32_little_endian(uint32_t x)
+vrr_u8abcd_from_u32(uint32_t x)
 {
     return (struct vrr_u8abcd){x, x >> 8, x >> 16, x >> 24};
 }
