89f0954d11416f31df2ce3416f99bff7df457c6d
Rupert Tombs <rupert.tombs@gmail.com>
2025-01-13 09:51:27 +0000

    Raw clock

--- README.md
+++ README.md
@@ -45,3 +45,7 @@ make time
     all files that may be included elsewhere.
   - To ensure only one set of `#include` directives in a build, undefine the guard
     with `#undef VRR_INCLUDE_GUARD` before your first set of include directives.
+- Clang-tidy disabled rules:
+  - `misc-include-cleaner` unnecessarily imports `bits/time.h` when `time.h` suffices.
+  - `readability-identifier-length` says that one character is always too short.
+  - `readability-magic-numbers` is too eager.
--- source/chrono.c
+++ source/chrono.c
@@ -1,28 +1,28 @@
 #ifdef VRR_INCLUDE_GUARD
 #undef VRR_INCLUDE_GUARD
-#include <time.h>       // struct timespec, clock_gettime, CLOCK_MONOTONIC
-#include <bits/time.h>  // FIXME(rupt): clang-tidy wants this
-#include <limits.h>     // LLONG_MAX
+#include <time.h>    // struct timespec, clock_gettime, CLOCK_MONOTONIC_RAW
+#include <limits.h>  // LLONG_MAX
 #include "chrono.h"
 #endif
 
 long long
-vrr_time_ns_best_of_50(void fn(void))
+vrr_time_ns_best_of_50(void (*const fn)(void))
 {
     long long best = LLONG_MAX;
     for (int i = 0; i < 50; ++i) {
         struct timespec start;
-        int const error_start = clock_gettime(CLOCK_MONOTONIC, &start);
+        int const error_start = clock_gettime(CLOCK_MONOTONIC_RAW, &start);
         fn();
         struct timespec end;
-        int const error_end = clock_gettime(CLOCK_MONOTONIC, &end);
+        int const error_end = clock_gettime(CLOCK_MONOTONIC_RAW, &end);
         if (error_start || error_end) {
             continue;
         }
-        long long const duration = end.tv_nsec - start.tv_nsec +
-                                   1000000000LL * (end.tv_sec - start.tv_sec);
-        if (duration < best) {
-            best = duration;
+        long long const duration_ns =
+            end.tv_nsec - start.tv_nsec +
+            (end.tv_sec - start.tv_sec) * 1000000000LL;
+        if (duration_ns < best) {
+            best = duration_ns;
         }
     }
     return best;
