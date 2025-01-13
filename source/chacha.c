b1b5492cfaa010edd6429668faab206b87ed558c
Rupert Tombs <rupert.tombs@gmail.com>
2025-01-13 09:55:05 +0000

    Tidy chacha

--- source/chacha.c
+++ source/chacha.c
@@ -5,9 +5,9 @@
 #include "chacha.h"
 #endif
 
-static inline struct vrr_u32x4x4 vrr_chacha_double_round(struct vrr_u32x4x4 x);
-static inline struct vrr_u32x4 vrr_chacha_quarter_round(uint32_t a, uint32_t b,
-                                                        uint32_t c, uint32_t d);
+static inline struct vrr_u32x4x4 vrr_chacha_double(struct vrr_u32x4x4 x);
+static inline struct vrr_u32x4 vrr_chacha_quarter(uint32_t a, uint32_t b,
+                                                  uint32_t c, uint32_t d);
 
 // TODO(rupt): only publicly expose the bytestream api.
 struct vrr_u32x4x4
@@ -15,7 +15,7 @@ vrr_chacha20(struct vrr_u32x4x4 const x)
 {
     struct vrr_u32x4x4 v = x;
     for (int i = 0; i < 10; ++i) {
-        v = vrr_chacha_double_round(v);
+        v = vrr_chacha_double(v);
     }
     return (struct vrr_u32x4x4){
         x.aa + v.aa, x.ba + v.ba, x.ca + v.ca, x.da + v.da,
@@ -26,28 +26,28 @@ vrr_chacha20(struct vrr_u32x4x4 const x)
 }
 
 static inline struct vrr_u32x4x4
-vrr_chacha_double_round(struct vrr_u32x4x4 const x)
+vrr_chacha_double(struct vrr_u32x4x4 const x)
 {
     // Even rounds go down columns.
-    struct vrr_u32x4 const a = vrr_chacha_quarter_round(x.aa, x.ab, x.ac, x.ad);
-    struct vrr_u32x4 const b = vrr_chacha_quarter_round(x.ba, x.bb, x.bc, x.bd);
-    struct vrr_u32x4 const c = vrr_chacha_quarter_round(x.ca, x.cb, x.cc, x.cd);
-    struct vrr_u32x4 const d = vrr_chacha_quarter_round(x.da, x.db, x.dc, x.dd);
+    struct vrr_u32x4 const a = vrr_chacha_quarter(x.aa, x.ab, x.ac, x.ad);
+    struct vrr_u32x4 const b = vrr_chacha_quarter(x.ba, x.bb, x.bc, x.bd);
+    struct vrr_u32x4 const c = vrr_chacha_quarter(x.ca, x.cb, x.cc, x.cd);
+    struct vrr_u32x4 const d = vrr_chacha_quarter(x.da, x.db, x.dc, x.dd);
     // Odd rounds go down diagonals.
-    struct vrr_u32x4 const va = vrr_chacha_quarter_round(a.a, b.b, c.c, d.d);
-    struct vrr_u32x4 const vb = vrr_chacha_quarter_round(b.a, c.b, d.c, a.d);
-    struct vrr_u32x4 const vc = vrr_chacha_quarter_round(c.a, d.b, a.c, b.d);
-    struct vrr_u32x4 const vd = vrr_chacha_quarter_round(d.a, a.b, b.c, c.d);
+    struct vrr_u32x4 const v1 = vrr_chacha_quarter(a.a, b.b, c.c, d.d);
+    struct vrr_u32x4 const v2 = vrr_chacha_quarter(b.a, c.b, d.c, a.d);
+    struct vrr_u32x4 const v3 = vrr_chacha_quarter(c.a, d.b, a.c, b.d);
+    struct vrr_u32x4 const v4 = vrr_chacha_quarter(d.a, a.b, b.c, c.d);
     return (struct vrr_u32x4x4){
-        va.a, vb.a, vc.a, vd.a,  // first row
-        vd.b, va.b, vb.b, vc.b,  // second row
-        vc.c, vd.c, va.c, vb.c,  // third row
-        vb.d, vc.d, vd.d, va.d,
+        v1.a, v2.a, v3.a, v4.a,  // format first row
+        v4.b, v1.b, v2.b, v3.b,  // format second row
+        v3.c, v4.c, v1.c, v2.c,  // format third row
+        v2.d, v3.d, v4.d, v1.d,
     };
 }
 
 static inline struct vrr_u32x4
-vrr_chacha_quarter_round(uint32_t a, uint32_t b, uint32_t c, uint32_t d)
+vrr_chacha_quarter(uint32_t a, uint32_t b, uint32_t c, uint32_t d)
 {
     a += b;
     d = vrr_rotate_left_u32(d ^ a, 16);
--- source/test_chacha.c
+++ source/test_chacha.c
@@ -10,14 +10,14 @@ main(void)
 {
     // It must match https://datatracker.ietf.org/doc/html/rfc7539 page 9.
     struct vrr_u32x4x4 const x = {
-        0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,  // first row
-        0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,  // second row
+        0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,  // format first row
+        0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,  // format second row
         0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
         0x00000001, 0x09000000, 0x4a000000, 0x00000000,
     };
     struct vrr_u32x4x4 const y = {
-        0xe4e7f110, 0x15593bd1, 0x1fdd0f50, 0xc47120a3,  // first row
-        0xc7f4d1c7, 0x0368c033, 0x9aaa2204, 0x4e6cd4c3,  // second row
+        0xe4e7f110, 0x15593bd1, 0x1fdd0f50, 0xc47120a3,  // format first row
+        0xc7f4d1c7, 0x0368c033, 0x9aaa2204, 0x4e6cd4c3,  // format second row
         0x466482d2, 0x09aa9f07, 0x05d7c214, 0xa2028bd9,
         0xd19c12b5, 0xb94e16de, 0xe883d0cb, 0x4e3c50a2,
     };
