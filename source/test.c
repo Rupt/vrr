901bf7b387af7de313d150cc62d6c0dda5d8d5fe
Rupert Tombs <rupert.tombs@gmail.com>
2025-03-01 14:01:02 +0000

    Condense checksum

--- source/test.c
+++ source/test.c
@@ -6,20 +6,21 @@
 #endif
 
 uint64_t
-vrr_checksum(unsigned long long const count, uint8_t const *const data)
+vrr_checksum(unsigned long long const count, uint8_t const *const bytes)
 {
     // Tabulation hashing with implicit tables from SplitMix64
     uint64_t sum = 0;
-    for (uint64_t i = 0; i < count; ++i) {
-        uint64_t x = (i << 8) | data[i];
+    // Mixing with a counter encodes each byte position.
+    // Counting from one distinguishes zero bytes from one zero-valued byte.
+    for (uint64_t i = 0, xi = 1 << 8; i < count; ++i, xi += 1 << 8) {
+        uint64_t x = xi | bytes[i];
         x *= 0x9e3779b97f4a7c15;
         x ^= x >> 30;
         x *= 0xbf58476d1ce4e5b9;
         x ^= x >> 27;
         x *= 0x94d049bb133111eb;
-        x ^= x >> 31;
-        // Accumulation with xor would make the last xor-shift redundant.
-        sum += x;
+        // Xor accumulation makes the final xor-shift redundant.
+        sum ^= x;
     }
     return sum;
 }
