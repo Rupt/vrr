295b8ee449ad4e5966a9aa9d3f37561e4ff09cdc
Rupert Tombs <rupert.tombs@gmail.com>
2025-03-03 14:01:16 +0000

    Back to big increment; improve language

--- source/test.c
+++ source/test.c
@@ -8,13 +8,13 @@
 uint64_t
 vrr_checksum(unsigned long long const count, uint8_t const *const bytes)
 {
-    // Tabulation hashing with implicit tables a MurmurHash3 finalizer variant
-    // Constants from David Stafford's Mix01
+    // Tabulation hashing with implicit tables from David Stafford's Mix01
     //  http://zimbry.blogspot.com/2011/09/better-bit-mixing-improving-on.html
     uint64_t sum = 0;
     // Mixing with a counter encodes each byte position.
-    // Counting from one distinguishes zero bytes from one zero-valued byte.
-    for (uint64_t i = 0, xi = 256; i < count; ++i, xi += 256) {
+    // Counting from nonzero distinguishes zero bytes from one zero-valued byte.
+    uint64_t const increment = 0x9e3779b97f4a7d00;
+    for (uint64_t i = 0, xi = increment; i < count; ++i, xi += increment) {
         uint64_t x = xi ^ bytes[i];
         x ^= x >> 31;
         x *= 0x7fb5d329728ea185;
