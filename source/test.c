f26f968664c72186030527e4868c70dd0b203cf8
Rupert Tombs <rupert.tombs@gmail.com>
2025-03-03 13:34:14 +0000

    Try different coefficients

--- source/test.c
+++ source/test.c
@@ -8,18 +8,19 @@
 uint64_t
 vrr_checksum(unsigned long long const count, uint8_t const *const bytes)
 {
-    uint64_t const c0 = 0x9e3779b97f4a7d00;
-    // Tabulation hashing with implicit tables from SplitMix64
+    // Tabulation hashing with implicit tables a MurmurHash3 finalizer variant
+    // Constants from David Stafford's Mix01
+    //  http://zimbry.blogspot.com/2011/09/better-bit-mixing-improving-on.html
     uint64_t sum = 0;
     // Mixing with a counter encodes each byte position.
     // Counting from one distinguishes zero bytes from one zero-valued byte.
-    for (uint64_t i = 0, xi = c0; i < count; ++i, xi += c0) {
+    for (uint64_t i = 0, xi = 256; i < count; ++i, xi += 256) {
         uint64_t x = xi ^ bytes[i];
-        x ^= x >> 30;
-        x *= 0xbf58476d1ce4e5b9;
-        x ^= x >> 27;
-        x *= 0x94d049bb133111eb;
         x ^= x >> 31;
+        x *= 0x7fb5d329728ea185;
+        x ^= x >> 27;
+        x *= 0x81dadef4bc2dd44d;
+        x ^= x >> 33;
         // Xor accumulation would make the final xor-shift redundant.
         sum += x;
     }
